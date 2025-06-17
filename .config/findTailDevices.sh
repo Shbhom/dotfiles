c4afe9be75343071db7e0b2e727577316f1593ff
Shubhom Srivastava <shbhoms@hornet.technology>
2025-06-17 16:22:09 +0530

    fix: Removed echo

--- .config/findTailDevices.sh
+++ .config/findTailDevices.sh
@@ -3,5 +3,4 @@
 ts=$(tailscale status --json)
 
 pick=$(echo "$ts" | jq -r '.Peer | values[] | .DNSName | split(".")[0]' | fzf)
-echo "SELECT DEVICE" "$pick"
 echo "$ts" | jq -r --arg name "$pick" '.Peer| values[]| select(.DNSName | startswith($name+"."))| {Host: .HostName, IPs: .TailscaleIPs, Online: .Online,Active: .Active}'
